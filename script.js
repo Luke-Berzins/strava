52fc76c58d97f4c02171384f3823fef988dd3659
Luke-Berzins <lukeberzins16@gmail.com>
2024-01-06 17:44:14 -0500

    dashboard css cleanup

--- script.js
+++ script.js
@@ -9,52 +9,67 @@ var colors = ['blue', 'green', 'red', 'purple', 'orange']; // Different colors f
 var isDrawing = false;
 var delay;
 var shouldZoom = true; // Flag for whether to zoom to each run
+function drawLineSegment(line, startPoint, endPoint, numSteps, stepDuration, onCompleted) {
+    let currentStep = 0;
+    let latDiff = (endPoint.lat - startPoint.lat) / numSteps;
+    let lngDiff = (endPoint.lng - startPoint.lng) / numSteps;
+
+    let lineSegmentInterval = setInterval(() => {
+        if (currentStep < numSteps) {
+            // Create the next point along the line
+            let nextPoint = new L.LatLng(
+                startPoint.lat + latDiff * currentStep,
+                startPoint.lng + lngDiff * currentStep
+            );
+            line.addLatLng(nextPoint);
+            currentStep++;
+        } else {
+            // Clear the interval and call the completion callback if provided
+            clearInterval(lineSegmentInterval);
+            if (typeof onCompleted === 'function') onCompleted();
+        }
+    }, stepDuration);
+}
 
 function drawRun(runIndex, pointIndex, line) {
-if (runIndex < runs.length && isDrawing) {
-var run = runs[runIndex];
+    if (runIndex < runs.length && isDrawing) {
+        var run = runs[runIndex];
+        
+        // Check if we're at the first point of the run
+        if (pointIndex === 0) {
+            // Create a new polyline
+            line = L.polyline([], {
+                color: colors[runIndex % colors.length],
+                weight: 4,
+                opacity: 0.6,
+                smoothFactor: 1
+            }).addTo(map);
 
-// If we're at the first point of the run, set up the initial view and polyline
-if (pointIndex === 0) {
-    var initialLatLng = new L.LatLng(run[0].latitude, run[0].longitude);
-    if (shouldZoom) {
-        map.setView(initialLatLng, parseInt(document.getElementById('run-zoom-input').value));
+            // If shouldZoom is true, set the map's view to the first point of the run
+            if (shouldZoom) {
+                map.setView(new L.LatLng(run[0].latitude, run[0].longitude), parseInt(document.getElementById('run-zoom-input').value));
+            }
+        }
+
+        // If there are more points to draw, draw the next segment
+        if (pointIndex < run.length - 1) {
+            let startPoint = new L.LatLng(run[pointIndex].latitude, run[pointIndex].longitude);
+            let endPoint = new L.LatLng(run[pointIndex + 1].latitude, run[pointIndex + 1].longitude);
+
+            // Draw the segment to the next point
+            drawLineSegment(line, startPoint, endPoint, 10, delay / 10, function() {
+                // Proceed to the next segment
+                drawRun(runIndex, pointIndex + 1, line);
+            });
+        } else {
+            // All points in the current run have been drawn, move to the next run
+            setTimeout(function() {
+                drawRun(runIndex + 1, 0, null);
+            }, delay);
+        }
     }
-    // Create a new polyline
-    line = L.polyline([initialLatLng], {
-        color: colors[runIndex % colors.length],
-        weight: 4,
-        opacity: 0.6,
-        smoothFactor: 1
-    }).addTo(map);
 }
 
-if (pointIndex < run.length) {
-    var point = run[pointIndex];
-    var latlng = new L.LatLng(point.latitude, point.longitude);
-
-    // Add the new point to the existing polyline
-    line.addLatLng(latlng);
-
-    // Schedule the next point to be drawn
-    setTimeout(function() {
-        drawRun(runIndex, pointIndex + 1, line);
-    }, delay);
-} else {
-    // Current run is finished, update the date display and move to the next run
-    var lastPointTime = new Date(run[run.length - 1].time);
-    var formattedDate = lastPointTime.getDate() + ' ' 
-                        + lastPointTime.toLocaleString('default', { month: 'long' }) + ' ' 
-                        + lastPointTime.getFullYear();
-    document.getElementById('date-display').innerText = 'Latest Date: ' + formattedDate;
-
-    // Move to the next run after a delay
-    setTimeout(function() {
-        drawRun(runIndex + 1, 0, null);
-    }, delay);
-}
-}
-}
 
 // To start drawing
 drawRun(0, 0, null);
